876a7894879b9b4bab7fd807cc8b063bb3e92bce
Shunkicreate <you@example.comjmsrsyunrinsyunki@gmail.com>
2021-09-18 00:35:03 +0900

    画像検索の大幅改善

--- src/components/Beautiful_gitl.vue
+++ src/components/Beautiful_gitl.vue
@@ -2,63 +2,33 @@
   <div class="bootstrap">
     <div class="container">
       <div class="row justify-content-center">
-        <div class="col-4 align-self-end">
+        <div class="col-auto col-md-12">
           <h1>こちらは美女を検索することのできるページです．</h1>
         </div>
-        <!-- <div class="col-4 align-self-left">
+        <div class="col-4">
           <img
             alt="Vue logo"
             class="img-fluid d-block mx-auto"
-            v-bind:src="img"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ45WpeeT5E0--ZNAmZkvrlAzAGc7BL7w1-GZsLkcnsUiiw483cr1u3zumxhg"
+            v-if="girl_search.type === 'default'"
           />
-        </div> -->
-        <div class="col-4 align-self-left">
           <img
             alt="Vue logo"
             class="img-fluid d-block mx-auto"
-            v-bind:src="imgs[num % 2]"
+            v-else-if="girl_search.type === 'api'"
+            :src="girl_search.img_url"
           />
+          <a :href=girl_search.img_url target="_blank">画像のリンクはこちら</a>
         </div>
-        <div class="col-4 align-self-left">
-          <img
-            alt="Vue logo"
-            class="img-fluid d-block mx-auto"
-            v-bind:src="img_url.data"
-          />
-        </div>
-        <div class="col-4 align-self-end">
-          <h1>こちらは美女を検索することのできるページです．</h1>
-        </div>
-        <!-- <div class="col-4">
-          <button
-            type="button"
-            class="btn btn-outline-secondary"
-            @click="change_img()"
-          >
-            検索
-          </button>
-        </div> -->
         <div class="col-4">
           <button
             type="button"
             class="btn btn-outline-secondary"
-            @click="num++"
-            v-bind:disabled="buttonstate"
+            @click="response_img()"
+            :disabled="girl_search.buttonstate"
           >
-            検索
+            {{ girl_search.searching }}
           </button>
-        </div>
-      </div>
-      <div class="col-4">
-        <button
-          type="button"
-          class="btn btn-outline-secondary"
-          @click="response_img()"
-        >
-          axios
-        </button>
-        <div>
-        {{img_url}}
         </div>
       </div>
     </div>
@@ -71,84 +41,49 @@ import axios from "axios";
 export default defineComponent({
   name: "bootstrap",
   setup() {
-    const API_URL = "https://hack-z-2021-9-prettygirl.azurewebsites.net/url";
-    const img = ref(
-      "https://d35omnrtvqomev.cloudfront.net/photo/article/article_header/thumbnail_image_path/21342/27642ae5002a8b23758c83760774ec.jpg"
-    );
-    const change_img = () => {
-      img.value =
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH_SqPTzj0qk7nQI8NXHMOILGHLvSJrytRXw&usqp=CAU";
-    };
-    const num = ref(0);
-    const imgs = reactive({
-      0: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ45WpeeT5E0--ZNAmZkvrlAzAGc7BL7w1-GZsLkcnsUiiw483cr1u3zumxhg&s",
-      1: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRH_SqPTzj0qk7nQI8NXHMOILGHLvSJrytRXw&usqp=CAU",
+    const API_URL = "https://hack-z-2021-9-prettygirl.azurewebsites.net";
+    const buttonstate = ref(false);
+    const img_url = ref("");
+    const type = ref("default");
+    const girl_search = reactive({
+      buttonstate: false,
+      img_url: "",
+      type: "default",
+      searching: "検索",
     });
-    const buttonstate=ref(true)
-
-
-
-    // const change_imgs = (imput_num) => {
-    //   console.log("before", imput_num);
-    //   if (imput_num == 0) {
-    //     imput_num = 1;
-    //   } else if (imput_num == 1) {
-    //     imput_num = 0;
-    //   }
-    //   console.log("after", imput_num);
-    // };
-
-
-
-    const img_url = ref(
-      "https://d35omnrtvqomev.cloudfront.net/photo/article/article_header/thumbnail_image_path/21342/27642ae5002a8b23758c83760774ec.jpg"
-    );
-
-
-  //  const img_url = ref(
-  //    "https://d35omnrtvqomev.cloudfront.net/photo/article/article_header/thumbnail_image_path/21342/27642ae5002a8b23758c83760774ec.jpg"
-  //  );
-
-    const back_img_url=ref("")
 
     // axios.<method> will now provide autocomplete and parameter typings
     const instance = {
-      // baseURL: "https://api.coindesk.com/v1/bpi/currentprice.json",
-      // baseURL: "https://hakadorikun.herokuapp.com",
-      // baseURL: "https://api.github.com",
-      baseURL: "https://hack-z-2021-9-prettygirl.azurewebsites.net",
+      baseURL: API_URL,
       timeout: 100000,
-      headers: { 'Content-type': 'text/json' },
+      headers: { "Content-type": "text/json" },
       // `withCredentials` indicates whether or not cross-site Access-Control requests
       // should be made using credentials
       withCredentials: false, // default
     };
+
     async function response_img() {
-      // img_url.value = await axios.get(API_URL,withCredentials=false);
-      // console.log(img_url.value);
-      // img_url.value = await axios.get('/users/octocat/orgs/octokit/repos',instance);
-      img_url.value = await axios.get('/url',instance).then(
-      );
-      
+      girl_search.buttonstate = true;
+      girl_search.searching = "検索中．．．";
+      await axios
+        .get("/url", instance)
+        .then(function (response) {
+          // handle success(axiosの処理が成功した場合に処理させたいことを記述)
+          girl_search.img_url = response.data;
+          girl_search.buttonstate = false;
+          girl_search.searching = "検索";
+        })
+        .catch(function (error) {
+          // handle error(axiosの処理にエラーが発生した場合に処理させたいことを記述)
+          console.log(error);
+        });
+      if (girl_search.type == "default") {
+        girl_search.type = "api";
+      }
     }
-    // onMounted(() => {
-    //     getRate()
-    // })
-
-
-
-
     return {
-      img,
-      change_img,
-
-      num,
-      imgs,
-      // change_imgs,
-      img_url,
-      back_img_url,
       response_img,
-      buttonstate,
+      girl_search,
     };
   },
 });
