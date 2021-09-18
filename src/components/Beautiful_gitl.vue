35070f669cf6a7a36038e6d949fc36366522227b
chanryu0811 <ryuhana.811@icloud.com>
2021-09-19 00:46:54 +0900

    アクア

--- src/components/Beautiful_gitl.vue
+++ src/components/Beautiful_gitl.vue
@@ -37,6 +37,19 @@
         </div>
       </div>
     </div>
+    <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
   </div>
 </template>
 
